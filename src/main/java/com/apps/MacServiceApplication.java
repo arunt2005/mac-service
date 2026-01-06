0913fa3d530b8ae599e5fe5e375ca66362872dcf
arunt2005 <arunt2005@gmail.com>
2026-01-06 14:27:51 +0530

    first commit

---
+++ src/main/java/com/apps/MacServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacServiceApplication.class, args);
+	}
+
+}
